ddace7ef9823b9ab76608d099980d81be9be6ebc
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-06 14:52:20 -0400

    up to 3.4 working

--- server.js
+++ server.js
@@ -12,7 +12,36 @@ const home = (req, res) => res.status(200).render("pages/homepage", { users });
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
+const userPage = (req, res) => {
+  const user_ID = req.params.userID;
+  console.log(user_ID);
+  if (
+    true
+    // parseInt(path[0]) >= 100 &&
+    // parseInt(path[0]) <= 125 &&
+    // path[0].length === 3
+  ) {
+    const user = users.find((element) => element._id === user_ID);
+    const friends = users.filter((element) =>
+      element.friends.includes(user_ID)
+    );
+    res.render("pages/profile", { user, friends });
+  } else {
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
+};
+const login = (req, res) => res.status(200).render("pages/signin", { users });
+const handleName = (req, res) => {
+  const userCheck = users.find(
+    (element) => element.name === req.query.firstName
+  );
+  console.log(userCheck);
 
+  userCheck != undefined ? res.redirect(`/users/${userCheck._id}`) : "";
+};
 // -----------------------------------------------------
 // server endpoints
 express()
@@ -23,6 +52,9 @@ express()
 
   // endpoints
   .get("/", home)
+  .get("/signin", login)
+  .get("/users/:userID", userPage)
+  .get("/getname", handleName)
 
   // a catchall endpoint that will send the 404 message.
   .get("*", handleFourOhFour)
