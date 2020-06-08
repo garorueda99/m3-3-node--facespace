cab83ebf099cbe82cb36034aa294148351445b74
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-08 18:38:25 -0400

    404 resolved

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
-const { users } = require('./data/users');
+const express = require("express");
+const morgan = require("morgan");
+const { users } = require("./data/users");
 
 let currentUser = undefined;
 
 const home = (req, res) =>
-  res.status(200).render('pages/homepage', { users, currentUser });
+  res.status(200).render("pages/homepage", { users, currentUser });
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
@@ -15,29 +15,21 @@ const handleFourOhFour = (req, res) => {
 
 const profilePage = (req, res) => {
   const user_ID = req.params.userID;
-  if (
-    true
-    // parseInt(path[0]) >= 100 &&
-    // parseInt(path[0]) <= 125 &&
-    // path[0].length === 3
-  ) {
+  if (users.find((element) => element._id === user_ID) != undefined) {
     const profile = users.find((element) => element._id === user_ID);
     const friends = users.filter((element) =>
       element.friends.includes(user_ID)
     );
-    res.render('pages/profile', { profile, friends, currentUser });
+    res.render("pages/profile", { profile, friends, currentUser });
   } else {
-    res.render('pages/fourOhFour', {
-      title: 'I got nothing',
-      path: req.originalUrl,
-    });
+    res.status(404).send("I couldn't find what you're looking for.");
   }
 };
 const login = (req, res) => {
   if (currentUser === undefined) {
-    res.status(200).render('pages/signin', { users, currentUser });
+    res.status(200).render("pages/signin", { users, currentUser });
   } else {
-    res.status(404).redirect('back');
+    res.status(404).redirect("back");
   }
 };
 
@@ -45,23 +37,23 @@ const handleName = (req, res) => {
   currentUser = users.find((element) => element.name === req.query.firstName);
   currentUser != undefined
     ? res.status(200).redirect(`/users/${currentUser._id}`)
-    : res.status(404).redirect('back');
+    : res.status(404).redirect("back");
 };
 // -----------------------------------------------------
 // server endpoints
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
-  .get('/', home)
-  .get('/signin', login)
-  .get('/users/:userID', profilePage)
-  .get('/getname', handleName)
+  .get("/", home)
+  .get("/signin", login)
+  .get("/users/:userID", profilePage)
+  .get("/getname", handleName)
 
   // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
+  .get("*", handleFourOhFour)
 
-  .listen(8000, () => console.log('Listening on port 8000'));
+  .listen(8000, () => console.log("Listening on port 8000"));
